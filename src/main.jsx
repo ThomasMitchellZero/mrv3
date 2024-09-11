7074a10bd82c74ecee5c3a692bef9416c78f93a9
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2024-09-11 19:29:50 -0400

    still here.

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='mrv'>
+    <BrowserRouter basename='/mrv'>
       <App />
     </BrowserRouter>
   </StrictMode>
