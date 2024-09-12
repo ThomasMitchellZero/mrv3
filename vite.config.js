78899db373d3bc4a8f526530c8f10ab2efa70e0d
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2024-09-12 12:04:00 -0400

    take a wild guess.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mrv2/", // Base URL for the app
+  base: "/", // Base URL for the app
   build: {
     outDir: "dist", // Output directory
   },
