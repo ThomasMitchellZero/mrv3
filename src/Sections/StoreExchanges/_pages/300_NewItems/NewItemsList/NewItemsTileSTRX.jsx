eb734d80ee72f43df92eee15648280272b9a6fbc
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2024-09-19 16:54:51 -0400

    Tile qty correctly updating.

--- src/Sections/StoreExchanges/_pages/300_NewItems/NewItemsList/NewItemsTileSTRX.jsx
+++ src/Sections/StoreExchanges/_pages/300_NewItems/NewItemsList/NewItemsTileSTRX.jsx
@@ -1,5 +1,6 @@
 import { MRVitemDetails } from "../../../../../mrv/mrv-components/DisplayOutputs/mrvItemDetails";
 import { BigLabeledValue } from "../../../../../mrv/mrv-components/DisplayOutputs/BigLabeledValue";
+import { MRVinput } from "../../../../../mrv/mrv-components/inputs/MRVinput";
 
 import { useOutlet } from "react-router-dom";
 import { useOutletContext } from "react-router";
@@ -17,9 +18,12 @@ import {
 function NewItemsTileSTRX({ itemAtom = new returnAtom({}) }) {
   const findAtom = useFindAtom();
   const mrvCtx = useOutletContext();
+  const setSessionItems = useSetSessionItems();
+  const locMethods = useLocStMethods_STRX().NewItems();
   const sessionMRV = mrvCtx.sessionMRV;
   const setSessionMRV = mrvCtx.setSessionMRV;
-  const locMethods = useLocStMethods_STRX().NewItems();
+  const newItems = sessionMRV.newItems;
+  const atomizedNewItems = sessionMRV.atomizedNewItems;
 
   const returnItemQty = findAtom({
     itemNum: itemAtom.atomItemNum,
@@ -27,21 +31,90 @@ function NewItemsTileSTRX({ itemAtom = new returnAtom({}) }) {
     asIndex: false,
   }).atomItemQty;
 
+  const tileQty = itemAtom.atomItemQty;
+  const qtyStatus = !returnItemQty
+    ? "noReturn"
+    : returnItemQty === tileQty
+    ? "valid"
+    : "incomplete";
+
+  // shared functions
+
+  const thisTileAtoms = atomizedNewItems.filter(
+    (atom) => atom.atomItemNum === itemAtom.atomItemNum
+  );
+
+  const handleItemQtyChange = (e) => {
+    const newQty = e.target.value;
+
+    setSessionItems({
+      itemsArrRouteStr: "newItems",
+      itemAtom: itemAtom,
+      newQty: newQty,
+      actionType: "edit",
+    });
+  };
+
+  // This complexity isn't needed yet, but it will be once we handle more transaction types.
+  const transTypeFilter = (transType) => {
+    return thisTileAtoms.filter((atom) => atom.atomTransType === transType);
+  };
+  const transTypeQty = (transTypeArr) => {
+    let outQty = 0;
+    outQty = transTypeArr.reduce((acc, curr) => {
+      return acc + curr.atomItemQty;
+    }, 0);
+    return outQty;
+  };
+
+  const oConfigs = {};
+
+  const mvpConfigs = {
+    noReturn: {
+      returnQty: "None",
+      status: "badRed",
+    },
+    valid: {
+      returnQty: returnItemQty,
+      status: "defaultBlack",
+    },
+    incomplete: {
+      returnQty: returnItemQty,
+      status: "defaultBlack",
+    },
+  };
+
+  // Backup in case we are mega-crunched pre-demo and we need to get this working.
+
   return (
     <div className={`tile tileSpan`}>
       <div className={`col itemSpan`}>
-        <MRVitemDetails thisItemAtom={itemAtom} />
+        <MRVitemDetails
+          showPrice={false}
+          showQty={false}
+          thisItemAtom={itemAtom}
+        />
       </div>
       <div className={`col tileInfoSpan`}>
         <div className={`col rtrnSpan centerAll`}>
           <BigLabeledValue
             labelStr={`Qty Returned`}
-            valueStr={returnItemQty}
+            valueStr={returnItemQty || 0}
             size={`M`}
-            status={`neutralGrey`}
+            status={`${mvpConfigs[qtyStatus].status}`}
           />
         </div>
-        <div className={`col qtySpan`}></div>
+        <div className={`col qtySpan centerAll`}>
+          <MRVinput>
+            <input
+              type={`number`}
+              value={tileQty}
+              onChange={(e) => {
+                handleItemQtyChange(e);
+              }}
+            />
+          </MRVinput>
+        </div>
         <div className={`col iconSpan`}></div>
       </div>
     </div>
