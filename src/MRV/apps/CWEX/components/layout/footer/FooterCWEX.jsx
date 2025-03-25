6e922d57094f3f5f0bf87b5a5664c8801761535c
Thomas Mitchell <thomas.mitchell.13@gmail.com>
2025-03-25 11:25:25 -0400

    Changed footer to an MRV component. This will eventually be reused across apps.

--- src/MRV/apps/CWEX/components/layout/footer/FooterCWEX.jsx
+++ src/MRV/apps/CWEX/components/layout/footer/FooterCWEX.jsx
@@ -1,73 +1,7 @@
-import "./FooterCWEX_style.css";
-
-import { centsToDollars } from "../../../../../mrv_controller";
+import { FooterMRV } from "../../../../../components/layout/footer/FooterMRV";
 
 function FooterCWEX({ sBtnLabel = "Continue" }) {
-  // In the real version, these vals will be mapped to the data.
-  const iCentReturnItemValue = -42000;
-  const iCentNewItemCost = 6900;
-  const iCentTotal = iCentReturnItemValue + iCentNewItemCost;
-
-  // Cash Breakdown ///////////////////////////////////////////////////
-
-  const aRowData = [
-    { sLabel: "Return Item Value:", iCentValue: -42000 },
-    { sLabel: "New Item Cost:", iCentValue: 6900 },
-  ];
-
-  const uiCashRow = (oRowData) => {
-    const sRowColor =
-      oRowData.iCentValue > 0 ? "color__primary__text" : "color__green__text";
-    return (
-      <div key={oRowData.sLabel} className={`cashRow ${sRowColor}`}>
-        <div className={`cashLabel body__medium`}>{oRowData.sLabel}</div>
-        <div className={`cashValue body__large bold`}>
-          {`$${centsToDollars(oRowData.iCentValue)}`}
-        </div>
-      </div>
-    );
-  };
-
-  const uiCashRowArr = aRowData.map((oThisRowData) => uiCashRow(oThisRowData));
-
-  // Total ///////////////////////////////////////////////////
-
-  const oTotalConfigs = {
-    refund: {
-      sLabel: "total refund:",
-      sColor: "color__green__text",
-    },
-    charge: {
-      sLabel: "total due:",
-      sColor: "color__primary__text",
-    },
-  };
-
-  const sActiveTotalKey = iCentTotal < 0 ? "refund" : "charge";
-  const oActiveTotal = oTotalConfigs[sActiveTotalKey];
-
-  // Button Container ///////////////////////////////////////////////////
-
-  const phActiveError = false;
-  const uiActiveError = phActiveError ? (
-    <div className={`warning`}>Error</div>
-  ) : null;
-
-  return (
-    <footer className={`footer footerCWEX`}>
-      <div className={`cashRowsCol`}>{uiCashRowArr}</div>
-      <div className={`cashTotalCol ${oActiveTotal.sColor}`}>
-        <div className={`body__small`}>{oActiveTotal.sLabel}</div>
-        <div className={`heading__large`}>
-          {`$${centsToDollars(iCentTotal)}`}
-        </div>
-      </div>
-      <div className={`btnCol`}>
-        <button className={`primary width__max jumbo`}>{sBtnLabel}</button>
-        {uiActiveError}
-      </div>
-    </footer>
-  );
+  return <FooterMRV sBtnLabel={sBtnLabel} />;
 }
 
 export { FooterCWEX };
